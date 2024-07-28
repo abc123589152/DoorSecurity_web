a01b5b0ced0ccc4273eecd00b63b1687f4364464
abc123589152 <70843940+abc123589152@users.noreply.github.com>
2024-07-28 17:30:01 +0800

    Update dbconnect_noparams.py

--- model/dbconnect_noparams.py
+++ model/dbconnect_noparams.py
@@ -1,10 +1,10 @@
 import mysql.connector as mysql
 def dbConnect_noparams(sqlCommand):
     db = mysql.connect(
-    host = '172.16.1.186',
-    user = "root",
-    passwd = "1qaz@WSX",
-    database = "DoorSecurity_demo",
+    host = 'your host ip',
+    user = "mysql user",
+    passwd = "mysql password",
+    database = "DoorSecurity_demo",#DataBase Name
     port = "13306"
     )
     ConnectDB = db.cursor()
